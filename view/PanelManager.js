bbf82c96c6e47a7b29be5ca416f9b565c786c95a
isaacs <isaacselement@gmail.com>
2013-06-26 19:46:17 +0800

    new version again

--- view/PanelManager.js
+++ view/PanelManager.js
@@ -1,35 +1,38 @@
 
 function Panel() {
     this.div = $('<div/>') ;
-    $('body').append(this.div);
+    $('body').append($('<div/>').append(this.div));
+    var cdToggle = true;
 
     var cssObj = {
         'position': 'relative',   // important!!!
-        'width' : '200px',
-        'height': '60%',
+        'float': 'left',
+//        'display': 'inline-block',
+//        'white-space': 'nowrap',
+        'width' : 'auto',
+        'height': 'auto',
         'margin':'0 auto',
         'border':'1px solid gray',
         'borderRadius':'3px',
         'boxShadow':'rgba(0, 0, 0, 0.5) 0 4px 18px',
-        'backgroundColor':'rgba(0,0,0,0.3)'       // to solved Child elements will opacity too
+        'backgroundColor':'rgba(255,255,255,0.01)'       // to solved Child elements will opacity too
 //        'opacity':'0.8',                          // Child elements will opacity too
     }
-    var cssObj1 = {
-    'position': 'relative',
-    'width': '100px',
-    'height': '100px',
-    'padding': '0.5em',
-    'float': 'left',
-    'margin': '0 10px 10px 0',
-    'background-color': 'red',
-    'border': '2px solid gray'
-}
 
     this.div.css(cssObj);
 
-    this.div.bind("click", function(event){
-        console.log('Click stop.....')
+//    this.div.bind("click", function(event){
+//        console.log('Click.....')
+//    });
+
+    this.div.click(function(event) {
+        console.log('Click.....');
+        if (cdToggle) {
+            console.log('The Click you known.....');
+        }
+        cdToggle = true;
     });
+
 
 //    this.div.attr('draggable','true');
 
@@ -84,20 +87,27 @@ function Panel() {
 //
 //    });
 
+
+    // http://stackoverflow.com/questions/6398854/jquery-draggable-with-ease
+
     this.div.draggable({
         // Can't use revert, as we animate the original object
         //revert: true,
 
-        //axis: "y",
         helper: function(){
             // Create an invisible div as the helper. It will move and
             // follow the cursor as usual.
             return $('<div></div>').css('opacity',0);
         },
+        start : function(event, ui) {
+            console.log('Drag start.....');
+            cdToggle = false;
+        },
         create: function(){
             // When the draggable is created, save its starting
             // position into a data attribute, so we know where we
             // need to revert to.
+            console.log('Drag create.....');
             var $this = $(this);
             $this.data('startPosition',$this.position());
         },
@@ -109,7 +119,7 @@ function Panel() {
             $this.stop().animate({
                 top: $this.data('startPosition').top,
                 left: $this.data('startPosition').left
-            },1000,'easeOutBounce');
+            },800,'easeOutBounce');
         },
         drag: function(event, ui){
             // During dragging, animate the original object to
@@ -122,5 +132,19 @@ function Panel() {
         }
 
     });
+
+    this.appendImage = function(image) {
+        this.div.append(image);
+    }
+
+    this.positionCenter = function() {
+        var parent = this.div.parent();
+
+        parent.css({
+            'position': 'absolute',
+            'top': '50%',
+            'left': '50%'
+        });
+    }
 
 }
--- view/ViewManager.js
+++ view/ViewManager.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    var cssObj = {
-        'width' : '100%',
-        'height': '100%'
-    }
-    $('body').css(cssObj);
     var panel = new Panel();
+
+    var image = new Image();
+    image.src = './resources/images/twitter.jpg'
+    panel.appendImage(image)
+    panel.positionCenter();
 });
